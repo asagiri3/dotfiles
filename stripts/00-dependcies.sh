20d0dcf572dac98f773dc8a8f083b6b6426ac77a
Asagiri <235324180+asagiri3@users.noreply.github.com>
2025-11-21 19:58:55 +0700

    idk what i doing

---
+++ stripts/00-dependcies.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# archlinux
+sudo pacman -Syu
+sudo pacman -S xwayland-statellite \
+    rofi \
+    wofi \
+    ghostty \
+    wezterm \
+    kitty \
+    matugen \
+    ttf-fira-code \
+    ttf-firacode-nerd \
+    vscodium \
+    neovim \
+    swww \
+    swaync \
+    rust \
+    nwg-look \
+    ninja \
+    clang \
+    cmake \
+    llvm \
+    kvantum \
+    fastfetch \
+    imagemagick \
+    hyprlock \
+    foot \
+    fontconfig \
+    fish \
+    firefox \
+    ffmpeg \
+    dolphin \
+    chezmoi \
+    flatpak \
+    btop \
+    brightnessctl \
+    breeze-icons \
+    adobe-source-han-sans-cn-fonts \
+    adobe-source-han-sans-jp-fonts \
+    adobe-source-han-sans-kr-fonts \
+    ripgrep \
+    mpv \
+    npm \
+    ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common \
+    unzip \
+    wtype \
+    unrar
+
+paru -S clipvault \
+    calibre-bin
+
+# fedora
+## bruh
