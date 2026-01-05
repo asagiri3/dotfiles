1b1c063caa27a74d3275d672a77b71a652d0df17
Maachue <109926858+maachue@users.noreply.github.com>
2026-01-05 19:00:00 +0700

    update

--- dot_config/eww/bar/scripts/executable_start.sh
+++ dot_config/eww/bar/scripts/executable_start.sh
@@ -3,19 +3,7 @@
 eww kill
 eww -c $HOME/.config/eww/bar kill
 
-state=$($HOME/.config/eww/scripts/index_ouputs.sh)
-line=$(printf '%s\n' "$state" | wc -l)
-line=$((line))
-
-# echo $line
-# echo $state
-
-if (($line < 2)); then
-    eww -c $HOME/.config/eww/bar open bar$(echo "$state" | head -n 1) --monitor 0
-    exit 0
-fi
-
 $HOME/.config/eww/scripts/index_ouputs.sh | while read name; do
-    echo "Starting bar$name on $name"
-    eww -c $HOME/.config/eww/bar open bar$name
+    echo "Starting \`bar$name\` on \`$name\`"
+    eww -c $HOME/.config/eww/bar open "bar$name" --screen "$name"
 done
