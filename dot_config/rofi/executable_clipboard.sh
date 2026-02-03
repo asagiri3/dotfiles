acaa1bb8bb8a9b73b77cfe0a9ec3e28549c56ed6
Maachue <109926858+maachue@users.noreply.github.com>
2026-02-03 13:18:59 +0700

    update

--- dot_config/rofi/executable_clipboard.sh
+++ dot_config/rofi/executable_clipboard.sh
@@ -7,17 +7,15 @@
 ## Extended for clipboard by : @maachue
 ##
 
-stlconf="$(<$HOME/.config/dots/style)"
-thmconf="$(<$HOME/.config/dots/theme)"
 clipmanager="$(<$HOME/.config/dots/clipboardmanager)"
 
 config="$stlconf"
 theme="$thmconf"
 
-dir="$HOME/.config/rofi/$config/$theme/clipboard"
+dir="$HOME/.config/rofi/clipboard"
 
 rofi \
     -modi clipboard:~/.config/rofi/scripts/${clipmanager}.sh \
     -show clipboard \
     -show-icons \
-    -theme ${dir}/main.rasi
+    -theme ${dir}/config.rasi
--- dot_config/rofi/executable_walselection.sh
+++ dot_config/rofi/executable_walselection.sh
@@ -10,13 +10,6 @@ if [[ "${WALLDIR: -1}" == "/" ]]; then
     WALLDIR="${WALLDIR%?}"
 fi
 
-stlconf="$(<$HOME/.config/dots/style)"
-thmconf="$(<$HOME/.config/dots/theme)"
-launcher="$(<$HOME/.config/dots/launcher/walsectype)"
-
-config="$stlconf"
-theme="$thmconf"
-
 entries=""
 for img in "$WALLDIR"/*; do
     [[ -f "$img" ]] || continue
@@ -24,10 +17,9 @@ for img in "$WALLDIR"/*; do
     entries+="$fname\0icon\x1f$img\n"
 done
 
-dir="$HOME/.config/rofi/$config/$theme/wallselect"
-mode="$launcher"
+dir="$HOME/.config/rofi/wallsec/config.rasi"
 
-choice=$(echo -e "$entries" | rofi -dmenu -theme ${dir}/${mode}.rasi)
+choice=$(echo -e "$entries" | rofi -dmenu -theme ${dir})
 choice="$WALLDIR/$choice"
 
 echo "$choice"
