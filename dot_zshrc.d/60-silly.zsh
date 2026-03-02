64609a16d0c0f0b37d2abca6c7eabab6a6e8d756
Maachue <109926858+maachue@users.noreply.github.com>
2026-03-02 18:49:26 +0700

    cleaning zshrc.d

--- dot_zshrc.d/60-silly.zsh
+++ dot_zshrc.d/60-silly.zsh
@@ -1,21 +1,26 @@
-silly_zen() { cbonsai -p; }
-silly_talk() { fortune | cowsay -f stegosaurus; }
-silly_sys() { fetch; }
+silly_zen()  { cbonsai -p }
+silly_talk() { fortune | cowsay -f stegosaurus }
+silly_sys()  { fetch }
 
-random_printing() {
-  modes=(silly_zen silly_talk silly_sys)
-  RAN=$((RANDOM % ${#modes[@]}))
-  ${modes[$RAN]}
+silly_printing() {
+  local mode_file="$HOME/.silly_mode"
+  [[ -f "$mode_file" ]] || return
+
+  local mode=$(<"$mode_file")
+  [[ "$mode" == "none" ]] && return
+
+  if [[ "$mode" == "random" ]]; then
+    local modes=(zen talk sys)
+    mode=${modes[$(( RANDOM % ${#modes[@]} + 1 ))]}
+  fi
+
+  case "$mode" in
+    zen)  silly_zen  ;;
+    talk) silly_talk ;;
+    sys)  silly_sys  ;;
+  esac
 }
 
 if [[ -z "$P9K_TTY" || "$P9K_TTY" == "$(tty)" ]] && [[ ! -v NVIM ]]; then
-  if [[ -f "$HOME/.silly_mode" ]]; then
-      case "$(cat "$HOME/.silly_mode")" in
-      zen)   silly_zen ;;
-      talk)  silly_talk ;;
-      sys)   silly_sys ;;
-      random) random_printing ;;
-      *) ;;
-      esac
-  fi
+  silly_printing
 fi
